c5caafcd7dbedcc4bd168f31c0c75c89e538295e
kumarAtmish <atmish.kumar@fivesdigital.com>
2022-02-01 02:38:55 -0800

    login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -47,7 +47,42 @@ router.post("/createuser", [
         res.json({authtoken})
     } catch (error) {
         console.log(error.message)
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal server error");
+    }
+})
+
+//Authenticate a user using: Post "/api/auth/login". No login required
+router.post("/login", [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!User){
+            return res.status(400).json({error: 'Please try to login with correct credentials'});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: 'Please try to login with correct credentials'});
+        }
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
+    } catch(error){
+        console.log(error.message)
+        res.status(500).send("Internal server error");
     }
 })
 
