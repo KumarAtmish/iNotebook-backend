0887a693c50882c23c03aba52f988d915edf287c
kumarAtmish <atmish.kumar@fivesdigital.com>
2022-02-07 21:32:41 -0800

    issues

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use('/api/notes', require('./src/routes/notes'))
 
 
 app.listen(port, () =>{
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
